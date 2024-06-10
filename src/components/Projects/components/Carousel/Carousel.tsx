d0f41f670a791c54fe5fc798a1dc140d77672759
ashanpw <ashan399@gmail.com>
2024-06-10 01:49:23 -0700

    fix: fix minor bugs in component UX

--- src/components/Projects/components/Carousel/Carousel.tsx
+++ src/components/Projects/components/Carousel/Carousel.tsx
@@ -1,9 +1,9 @@
 import useEmblaCarousel from "embla-carousel-react";
 import {
-  EmblaContainer,
-  EmblaDiv,
-  EmblaSlide,
-  EmblaViewport,
+    EmblaContainer,
+    EmblaDiv,
+    EmblaSlide,
+    EmblaViewport,
 } from "./Carousel.styles";
 
 import "./Carousel.scss";
@@ -11,50 +11,54 @@ import { useCallback, useState } from "react";
 import { Dots } from "../NavigationDots/NavigationDots";
 
 export const Carousel = (props: any) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+    const [emblaRef, emblaApi] = useEmblaCarousel();
 
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev();
-  }, [emblaApi]);
+    const scrollPrev = useCallback(() => {
+        if (emblaApi) emblaApi.scrollPrev();
+    }, [emblaApi]);
 
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext();
-  }, [emblaApi]);
+    const scrollNext = useCallback(() => {
+        if (emblaApi) emblaApi.scrollNext();
+    }, [emblaApi]);
 
-  // Sets the selected index
-  const [selectedIdx, setSelectedIdx] = useState(0);
-  emblaApi?.on("select", () => setSelectedIdx(emblaApi?.selectedScrollSnap));
+    // Sets the selected index
+    const [selectedIdx, setSelectedIdx] = useState(0);
+    emblaApi?.on("select", () => setSelectedIdx(emblaApi?.selectedScrollSnap));
 
-  const scrollToSlide = (idx: number): void => {
-    emblaApi?.scrollTo(idx);
-  };
+    const scrollToSlide = (idx: number): void => {
+        emblaApi?.scrollTo(idx);
+    };
 
-  return (
-    <div className="project-item-container">
-      <div ref={emblaRef} className="embla-viewport">
-        <div
-          className={
-            props.isMobile
-              ? "embla-container-mobile"
-              : "embla-container-desktop"
-          }
-        >
-          <EmblaSlide key={1}>
-            <video
-              src="https://ashanpw-asset-bucket.s3.amazonaws.com/ProjectWindOpening.mp4"
-              width="100%"
-              height="100%"
-              autoPlay={true}
-              controls={false}
-              loop={true}
-              muted={true}
+    return (
+        <div className="project-item-container">
+            <div ref={emblaRef} className="embla-viewport">
+                <div
+                    className={
+                        props.isMobile
+                            ? "embla-container-mobile"
+                            : "embla-container-desktop"
+                    }
+                >
+                    <EmblaSlide key={1}>
+                        <video
+                            src="https://ashanpw-asset-bucket.s3.amazonaws.com/ProjectWindOpening.mp4"
+                            width="100%"
+                            height="100%"
+                            autoPlay={true}
+                            controls={false}
+                            loop={true}
+                            muted={true}
+                        />
+                    </EmblaSlide>
+                    <EmblaSlide key={2}>Slide 2</EmblaSlide>
+                    <EmblaSlide key={3}>Slide 3</EmblaSlide>
+                </div>
+            </div>
+            <Dots
+                selectedIdx={selectedIdx}
+                size={3}
+                onClickFn={scrollToSlide}
             />
-          </EmblaSlide>
-          <EmblaSlide key={2}>Slide 2</EmblaSlide>
-          <EmblaSlide key={3}>Slide 3</EmblaSlide>
         </div>
-      </div>
-      <Dots selectedIdx={selectedIdx} size={3} onClickFn={scrollToSlide} />
-    </div>
-  );
+    );
 };
--- src/text/TextData.ts
+++ src/text/TextData.ts
@@ -1,151 +1,148 @@
 export const TextData = {
-  hero: {
-    headings: ["SOFTWARE ENGINEER", "PORTFOLIIO 2023", "ASHAN"],
-  },
-  about: {
-    details: [
-      { header: "TITLE", value: "Software Development Engineer II" },
-      { header: "EXPERIENCE", value: "3 Years" },
-      { header: "DOMAIN", value: "Full-Stack, Cloud, DevOps" },
-      { header: "SCHOOL", value: "University of Maryland, College Park" },
-      {
-        header: "HOBBIES",
-        value: "Building PCs, online games, & centering divs",
-      },
-      { header: "DEGREE", value: "Bachelor of Science" },
-    ],
-    name: "ASHAN",
-    titleList: ["SDE", "3 YOE", "Virginia", "—", "USA"],
-    description:
-      "Hi, I'm Ashan, a software engineer with over 3 years of experience. I've built systems from scratch, covering both frontend and backend development. My focus is on precision and navigating the dynamic tech landscape.",
-  },
-  placesVisited: [
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_0430-min.jpg",
-      location: "Dotonbori, Japan",
-      alt: "Dotonbori, Japan",
+    hero: {
+        headings: ["SOFTWARE ENGINEER", "PORTFOLIIO 2023", "ASHAN"],
     },
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_0759-min.jpg",
-      location: "Nakatsugawa, Japan",
-      alt: "Fushimi Inari Shrine, Japan",
-    },
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_9840-min.jpg",
-      location: "Fushimi Inari Shrine, Japan",
-      alt: "Fushimi Inari Shrine, Japan",
-    },
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_2620-min.jpg",
-      location: "Shirakawa-go, Japan",
-      alt: "some image",
-    },
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_2951-min.jpg",
-      location: "Teamlabs Borderless, Japan",
-      alt: "some image",
-    },
-
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_8120-min.jpg",
-      location: "Seoul, Korea",
-      alt: "some image",
-    },
-    {
-      src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_3096-min.jpg",
-      location: "Teamlabs Borderless, Japan",
-      alt: "some image",
-    },
-
-  ],
-  tech: [
-    {
-      title: "FRONTEND",
-      technologies: [
-        "React",
-        "NextJS",
-        "Redux",
-        "TypeScript",
-        "Playwright",
-        "NodeJS",
-        "Storybook",
-        "Jest / Enzyme",
-        "React Spring",
-        "Framer Motion",
-        "Styled Components",
-        "RJFS",
-      ],
-    },
-    {
-      title: "BACKEND",
-      technologies: [
-        "JAX-RS / Jersey",
-        "GraphQL",
-        "aREST",
-        "MockK",
-        "JUnit",
-        "Apache / HTTP",
-        "Log4j",
-        "Guice / Dagger",
-        "Lombok",
-      ],
-    },
-    {
-      title: "CLOUD",
-      technologies: [
-        "Containers (AWS ECS / Fargate)",
-        "Docker",
-        "AWS Lambda",
-        "AWS CDK",
-        "Pipelines / Automation",
-        "Integration Testing",
-      ],
-    },
-    {
-      title: "ML / AI",
-      technologies: ["PyTorch", "TensorFlow"],
-    },
-  ],
-  experience: [
-    {
-      company: "AMAZON",
-      title: "SOFTWARE DEV. ENGINEER I",
-      summary:
-        "Key member of an Amazon game sales initiative, responsible for constructing React frontend components and a Kotlin-GraphQL backend. Developed an efficient one-click checkout process by building and integrating frontend and backend systems, utilizing GraphQL for customer payment retrieval and seamless payment method updates on the website",
-      team: "AWS App Mesh",
-      timeline: "June - August 2019",
-      imgUrl:
-        "https://ashanpw-asset-bucket.s3.amazonaws.com/Games+Wallpaper.png",
-      imgAlt: "Image showing Amazon product website for a video game",
-    },
-    {
-      company: "AMAZON",
-      title: "SOFTWARE DEV. ENGINEER II",
-      summary:
-        "Riot Games, the company that acts as the main distributor of the video game, has been in charge of managing video game, has been in charge of Riot Games, the company that acts as the main distributor of the video game, has been in charge of managing video game, has been in charge of ",
-      team: "AWS App Mesh",
-      timeline: "June - August 2019",
-      imgUrl:
-        "https://fastly.picsum.photos/id/675/400/500.jpg?hmac=2X5NSSStMEerYxgHCfn6vHVAbWFT2X8bbn36EsQvl0Y",
-      imgAlt: "Image showing Amazon product website for a video game",
-    },
-  ],
-  projects: {
-    desktopItems: [
-      {
-        title: "DRIVER DROWSINESS DETECTION",
-        years: "JUNE - AUGUST 2019",
+    about: {
+        details: [
+            { header: "TITLE", value: "Software Development Engineer II" },
+            { header: "EXPERIENCE", value: "3 Years" },
+            { header: "DOMAIN", value: "Full-Stack, Cloud, DevOps" },
+            { header: "SCHOOL", value: "University of Maryland, College Park" },
+            {
+                header: "HOBBIES",
+                value: "Building PCs, online games, & centering divs",
+            },
+            { header: "DEGREE", value: "Bachelor of Science" },
+        ],
+        name: "ASHAN",
+        titleList: ["SDE", "3 YOE", "Virginia", "—", "USA"],
         description:
-          "Under the supervision of Professor Mohamed Teli, worked with students Hyekang and Krishan to build a real-time driver drowsiness detection system using an arduino, a camera, and machine learning.",
-      },
+            "Hi, I'm Ashan, a software engineer with over 3 years of experience. I've built systems from scratch, covering both frontend and backend development. My focus is on precision and navigating the dynamic tech landscape.",
+    },
+    placesVisited: [
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_0430-min.jpg",
+            location: "Dotonbori, Japan",
+            alt: "Dotonbori, Japan",
+        },
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_0735-min.jpg",
+            location: "Nakatsugawa, Japan",
+            alt: "Fushimi Inari Shrine, Japan",
+        },
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_9840-min.jpg",
+            location: "Fushimi Inari Shrine, Japan",
+            alt: "Fushimi Inari Shrine, Japan",
+        },
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_2620-min.jpg",
+            location: "Shirakawa-go, Japan",
+            alt: "some image",
+        },
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_2951-min.jpg",
+            location: "Teamlabs Borderless, Japan",
+            alt: "some image",
+        },
+
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_8120-min.jpg",
+            location: "Seoul, Korea",
+            alt: "some image",
+        },
+        {
+            src: "https://ashanpw-asset-bucket.s3.amazonaws.com/camera-photos/DSC_3096-min.jpg",
+            location: "Teamlabs Borderless, Japan",
+            alt: "some image",
+        },
     ],
-    mobileItems: [
-      {
-        title: "DRIVER DROWSINESS DETECTION",
-        years: "JUNE - AUGUST 2019",
-        description:
-          "Under the supervision of Professor Mohamed Teli, worked with students Hyekang and Krishan to build a real-time driver drowsiness detection system using an arduino, a camera, and machine learning.",
-      },
+    tech: [
+        {
+            title: "FRONTEND",
+            technologies: [
+                "React",
+                "NextJS",
+                "Redux",
+                "TypeScript",
+                "Playwright",
+                "NodeJS",
+                "Storybook",
+                "Jest / Enzyme",
+                "React Spring",
+                "Framer Motion",
+                "Styled Components",
+                "RJFS",
+            ],
+        },
+        {
+            title: "BACKEND",
+            technologies: [
+                "JAX-RS / Jersey",
+                "GraphQL",
+                "aREST",
+                "MockK",
+                "JUnit",
+                "Apache / HTTP",
+                "Log4j",
+                "Guice / Dagger",
+                "Lombok",
+            ],
+        },
+        {
+            title: "CLOUD",
+            technologies: [
+                "Containers (AWS ECS / Fargate)",
+                "Docker",
+                "AWS Lambda",
+                "AWS CDK",
+                "Pipelines / Automation",
+                "Integration Testing",
+            ],
+        },
+        {
+            title: "ML / AI",
+            technologies: ["PyTorch", "TensorFlow"],
+        },
     ],
-  },
+    experience: [
+        {
+            company: "AMAZON",
+            title: "SOFTWARE DEV. ENGINEER I",
+            summary:
+                "Key member of an Amazon game sales initiative, responsible for constructing React frontend components and a Kotlin-GraphQL backend. Developed an efficient one-click checkout process by building and integrating frontend and backend systems, utilizing GraphQL for customer payment retrieval and seamless payment method updates on the website",
+            team: "AWS App Mesh",
+            timeline: "June - August 2019",
+            imgUrl: "https://ashanpw-asset-bucket.s3.amazonaws.com/Games+Wallpaper.png",
+            imgAlt: "Image showing Amazon product website for a video game",
+        },
+        {
+            company: "AMAZON",
+            title: "SOFTWARE DEV. ENGINEER II",
+            summary:
+                "Riot Games, the company that acts as the main distributor of the video game, has been in charge of managing video game, has been in charge of Riot Games, the company that acts as the main distributor of the video game, has been in charge of managing video game, has been in charge of ",
+            team: "AWS App Mesh",
+            timeline: "June - August 2019",
+            imgUrl: "https://fastly.picsum.photos/id/675/400/500.jpg?hmac=2X5NSSStMEerYxgHCfn6vHVAbWFT2X8bbn36EsQvl0Y",
+            imgAlt: "Image showing Amazon product website for a video game",
+        },
+    ],
+    projects: {
+        desktopItems: [
+            {
+                title: "DRIVER DROWSINESS DETECTION",
+                years: "JUNE - AUGUST 2019",
+                description:
+                    "Under the supervision of Professor Mohamed Teli, worked with students Hyekang and Krishan to build a real-time driver drowsiness detection system using an arduino, a camera, and machine learning.",
+            },
+        ],
+        mobileItems: [
+            {
+                title: "DRIVER DROWSINESS DETECTION",
+                years: "JUNE - AUGUST 2019",
+                description:
+                    "Under the supervision of Professor Mohamed Teli, worked with students Hyekang and Krishan to build a real-time driver drowsiness detection system using an arduino, a camera, and machine learning.",
+            },
+        ],
+    },
 };
